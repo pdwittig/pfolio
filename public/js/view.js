117bea9dc1d8c0780da101219d0ca3bb9baaca2c
Phil Wittig <pdwittig@gmail.com>
2014-06-11 15:57:26 -0700

    add nav fade in

--- public/js/view.js
+++ public/js/view.js
@@ -1,6 +1,8 @@
 function View() {
   this.introContainerSelector = '.inner-intro';
   this.introContentSelector = '.inner-intro div';
+  this.navbarSelector = '#navbar'
+  this.connectContainerSelector = '.connect'
 }
 
 View.prototype = {
@@ -11,14 +13,32 @@ View.prototype = {
   handleScroll: function() {
     var introContainer = document.querySelector( this.introContainerSelector );
     var introContent = document.querySelector( this.introContentSelector );
+    var navbar = document.querySelector( this.navbarSelector );
+    // var connectContainer = document.querySelector( this.connectContainerSelector );
 
     var newHeight = introContainer.clientHeight - window.scrollY;
-    var newHeightRatio = (newHeight - 150) / ( 700 - 150 );
+    var newHeightRatio = ( newHeight - 75 ) / ( 700 - 75 );
+    console.log("scrollY: " + window.scrollY)
+    // var connectOpacity = ( newHeight - 595 ) / 100;
 
-    console.log(newHeight );    
+    // console.log("conect opacity " + connectOpacity)
+
+    console.log("new height: " + newHeight );    
     console.log(newHeightRatio);
 
     introContainer.style.clip = "rect(0px, 1920px, " + newHeight + "px, 0px";
     introContent.style.opacity = newHeightRatio;
+
+    // connectContainer.style.opacity = connectOpacity;
+
+    if (newHeight < 50) {
+      opacity = ( 30 - newHeight ) / 30;
+      navbar.style.opacity = opacity;
+    }
+
+    // if ( window.scrollY > 1550 ) {
+    //   console.log("fucxk yoiu")
+    //   document.querySelector('li').style.color = "#34495E"
+    // }
   }
 }
