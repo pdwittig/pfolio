13a1819491bc68caccdac58949a7c284baa75d46
Phil Wittig <pdwittig@gmail.com>
2014-06-17 14:11:03 -0700

    refactor js and align monitor

--- public/js/view.js
+++ public/js/view.js
@@ -1,8 +1,16 @@
 function View() {
   this.introContainerSelector = '.inner-intro';
   this.introContentSelector = '.inner-intro div';
-  this.navbarSelector = '#navbar'
-  this.connectContainerSelector = '.connect'
+  this.navbarSelector = '#navbar';
+  this.connectContainerSelector = '.connect';
+  this.monitorPosition = 800;
+  this.windowDelta = 0;
+  this.lastWindowScrollY = 0;
+  
+  this.introContainerToTop = 0;
+  this.introContainerDisplayRatio = 0;
+  this.introContainer;
+  this.introContent;
 }
 
 View.prototype = {
@@ -11,34 +19,40 @@ View.prototype = {
   },
 
   handleScroll: function() {
-    var introContainer = document.querySelector( this.introContainerSelector );
-    var introContent = document.querySelector( this.introContentSelector );
+    this.introContainer = document.querySelector( this.introContainerSelector );
+    this.introContent = document.querySelector( this.introContentSelector );
+    var introContainerHeight = this.introContainer.clientHeight;
+
+    this.introContainerToTop = introContainerHeight - window.scrollY;
+    this.introContainerDisplayRatio = ( this.introContainerToTop - 75 ) / ( introContainerHeight - 75 );
+
+    this.updateIntroContainer();
+    this.updateNavbar();
+    this.updateMonitor();
+
+  },
+
+  updateIntroContainer: function() {
+    this.introContainer.style.clip = "rect(0px, 1920px, " + this.introContainerToTop + "px, 0px";
+    this.introContent.style.opacity = this.introContainerDisplayRatio;
+  },
+
+  updateNavbar: function() {
     var navbar = document.querySelector( this.navbarSelector );
-    // var connectContainer = document.querySelector( this.connectContainerSelector );
-
-    var newHeight = introContainer.clientHeight - window.scrollY;
-    var newHeightRatio = ( newHeight - 75 ) / ( 700 - 75 );
-    console.log("scrollY: " + window.scrollY)
-    // var connectOpacity = ( newHeight - 595 ) / 100;
-
-    // console.log("conect opacity " + connectOpacity)
-
-    console.log("new height: " + newHeight );    
-    console.log(newHeightRatio);
-
-    introContainer.style.clip = "rect(0px, 1920px, " + newHeight + "px, 0px";
-    introContent.style.opacity = newHeightRatio;
-
-    // connectContainer.style.opacity = connectOpacity;
-
-    if (newHeight < 50) {
-      opacity = ( 30 - newHeight ) / 30;
+    if (this.introContainerToTop < 50) {
+      opacity = ( 30 - this.introContainerToTop ) / 30;
       navbar.style.opacity = opacity;
     }
+  },
 
-    // if ( window.scrollY > 1550 ) {
-    //   console.log("fucxk yoiu")
-    //   document.querySelector('li').style.color = "#34495E"
-    // }
+  updateMonitor: function() {
+    var monitor = document.querySelector( '.monitor-container' )
+    console.log("Scroll Y: " + window.scrollY)
+    if ( window.scrollY < 550 ) {
+      monitor.style.top = (800 - window.scrollY).toString() + "px";
+    }
+    else if ( window.scrollY > 550 ) {
+      monitor.style.top = "250px"
+    }
   }
 }
