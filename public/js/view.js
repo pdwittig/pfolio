e17bc80582d877d479621c1a0ff5ab9f63c68ef3
Phil Wittig <pdwittig@gmail.com>
2014-06-17 22:27:26 -0700

    WIP get project info scrolling

--- public/js/view.js
+++ public/js/view.js
@@ -3,7 +3,7 @@ function View() {
   this.introContentSelector = '.inner-intro div';
   this.navbarSelector = '#navbar';
   this.connectContainerSelector = '.connect';
-  this.monitorPosition = 800;
+  this.monitorPosition = 1000;
   this.windowDelta = 0;
   this.lastWindowScrollY = 0;
   
@@ -29,11 +29,14 @@ View.prototype = {
     this.updateIntroContainer();
     this.updateNavbar();
     this.updateMonitor();
+    this.updateProjectContainers();
+    this.updateProjectImages();
+    console.log(window.scrollY)
 
   },
 
   updateIntroContainer: function() {
-    this.introContainer.style.clip = "rect(0px, 1920px, " + this.introContainerToTop + "px, 0px";
+    this.introContainer.style.clip = "rect(0px, 1920px, " + this.introContainerToTop + "px, 0px)";
     this.introContent.style.opacity = this.introContainerDisplayRatio;
   },
 
@@ -46,13 +49,51 @@ View.prototype = {
   },
 
   updateMonitor: function() {
-    var monitor = document.querySelector( '.monitor-container' )
-    console.log("Scroll Y: " + window.scrollY)
-    if ( window.scrollY < 550 ) {
-      monitor.style.top = (800 - window.scrollY).toString() + "px";
+    var monitor = document.querySelectorAll( '.monitor-container' )
+    this.manageYPos( monitor )
+  },
+
+  updateProjectContainers: function() {
+    var gistboard = document.querySelector('#gistboard')
+    gistboard.style.clip = "rect(158px, 238px, 158px, 0px)";
+
+    var textCols = document.querySelectorAll('.text-col')
+    this.manageYPos( textCols )
+  },
+
+  updateProjectImages: function() {
+    var trala = document.querySelector('#trala')
+    var gistBoard = document.querySelector('#gistboard')
+    if ( window.scrollY > 1233 && window.scrollY < 1394 ) {
+      var scrollDelta = window.scrollY - 1233
+      // trala.style.opacity = ( 158 - scrollDelta ) / 158 
+      gistboard.style.clip = "rect(" + ( 158 - scrollDelta ).toString() + "px, 238px, 158px, 0px)";
+      trala.style.clip = "rect(" + ( 0 - scrollDelta ).toString() + "px, 238px, 158px, 0px)";
+
     }
-    else if ( window.scrollY > 550 ) {
-      monitor.style.top = "250px"
+  },
+
+  manageYPos: function(elements) {
+    if ( window.scrollY < 700 ) {
+      this.setYpos( elements, 1000 - window.scrollY )
+    }
+    else if ( window.scrollY > 700 ) {
+      this.setYpos( elements, 300 ) 
+    }
+  },
+
+  setYpos: function(elements, yPos) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.top = yPos.toString() + "px";
     }
   }
 }
+
+
+
+
+
+
+
+
+
